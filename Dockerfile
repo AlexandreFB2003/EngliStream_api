0744a50bdd9fb1e120d792e11c23a819f50dd096
GGVidal <gabrielvidalgondim@gmail.com>
2024-09-05 16:37:49 -0300

    chore: adding docker config and knex

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,8 @@ RUN npm install -g nodemon
 # Copy the rest of the application code
 COPY . .
 
+COPY .env .env
+
 # Expose the application port
 EXPOSE 3000
 
---
+++ knexfile.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./migrations",
+      extension: "ts", // Important for TypeScript migrations
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./migrations",
+      extension: "ts",
+    },
+  },
+};
+
+export default config;
